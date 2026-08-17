bc2ccae0a15dc3d59eb3c91b8fdec0b73f647249
Racketsvibes <racketsvibes@example.com>
2026-08-18 01:02:29 +0500

    feat: implement Grow by Mediavine script in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -130,6 +130,11 @@ export default function RootLayout({
             gtag('config', 'G-LYQ8CDZET1');
           `}
         </Script>
+        <Script id="grow-initializer" strategy="afterInteractive" data-grow-initializer="">
+          {`
+            !(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTo4OGNjNWJhOC02NDZlLTQxMzYtYWIzYS01YzIwMTk0ZTcyMWQ=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();
+          `}
+        </Script>
         <Header />
         <main id="main">{children}</main>
         <Footer />
